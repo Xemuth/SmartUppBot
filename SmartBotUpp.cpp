ce906becf25e367d934853a38d2a1a58689f3a95
Felix SOIGNON <felix.soignon@niji.fr>
2019-04-29 10:18:35 +0200

    Ajout SQL Lite 3

--- SmartBotUpp.cpp
+++ SmartBotUpp.cpp
@@ -1,5 +1,12 @@
 #include <Core/Core.h>
 #include <DiscordUpp/Discord.h>
+#include <SmartUppBot/sql_lite.h>
+
+#ifdef _DEBUG
+#include <Sql/sch_schema.h>
+#endif
+
+#include <Sql/sch_source.h>
 
 using namespace Upp;
 
@@ -16,56 +23,98 @@ CONSOLE_APP_MAIN {
         String id = payload["d"]["author"]["id"];
         String discriminator = payload["d"]["author"]["discriminator"];
         
+        String battletag;
         ValueMap json;
         
-        Cout() << "Content : " << content<< "\n";
-	        /*if(content.Find("314391413200650250")!=-1){
-	            bot.CreateMessage(channel, String("<@"+ id + "> tu es un idiot !") );
-	        }
-	        else if(userName.Compare("Bot_Hendek")!=0){
-	        	bot.CreateMessage(channel, String("Je suis vivant !") );
-	        }*/
+        //Cout() << "Content : " << content<< "\n";
 	        
+	        //Affichage des stats d'un BattleTag
 	        if (content.Find("!stats")!=-1){
 	        	content.Remove(0, 7);
-	        	String battletag = content;
-	        	
-	        	
-	        	
-	        	if (userName.Find("Xemuth")!=-1){
-	        		bot.CreateMessage(channel, "Je répond pas à toi");
-	        		return;
-	        	}
-	        	else if(userName.Find("Natty")!=-1){
-	        		bot.CreateMessage(channel, "Bonjour Maître");
-	        	}
-	        	
-	        	if (battletag.Find("BASTION") != -1){
-	        		bot.CreateMessage(channel, "Recherche de stats pour l'autre salope");
-	        	}
-	        	else{
-					bot.CreateMessage(channel, "Recherche de stats pour " + battletag);
-	        	}
+	        	battletag = content;
+	       		
+			  	bot.CreateMessage(channel, "Recherche de stats pour " + battletag);
+	        		        	
 				HttpRequest query("https://ow-api.com/v1/stats/pc/eu/" + battletag + "/profile");
+				query.GET();
+				query.Timeout(10000);
+				
+				json = ParseJSON(query.Execute());
+	        	bot.CreateMessage(channel,json["rating"]);
+	       
+	        }//MAJ des stats d'un BattleTag
+	        else if(content.Find("!upd")!=-1){
+ 	      		content.Remove(0, 5);
+ 	      		battletag = content;
+ 	      		
+ 	      		//bot.CreateMessage(channel, "Mise à jours des stats pour " + battletag);
+	        	
+	        	HttpRequest query("https://ow-api.com/v1/stats/pc/eu/" + battletag + "/profile");
 				query.GET();
 				query.Timeout(5000);
 				
 				json = ParseJSON(query.Execute());
+	        	
+	        	Sqlite3Session sqlite3;
+				sqlite3.LogErrors(true);
+				if(!sqlite3.Open(ConfigFile("simple.db"))) {
+					Cout() << "Can't create or open database file\n";
+					return;
+				}
 				
-				Cout() << json["rating"] << " -> rating \n";
+				SQL = sqlite3;
 				
-				Cout() << ~json["rating"]<< "\n";
+				#ifdef _DEBUG
+					SqlSchema sch(SQLITE3);
+					All_Tables(sch);
+					if(sch.ScriptChanged(SqlSchema::UPGRADE))
+						SqlPerformScript(sch.Upgrade());
+					if(sch.ScriptChanged(SqlSchema::ATTRIBUTES)) {
+						SqlPerformScript(sch.Attributes());
+					}
+					if(sch.ScriptChanged(SqlSchema::CONFIG)) {
+						SqlPerformScript(sch.ConfigDrop());
+						SqlPerformScript(sch.Config());
+					}
+					sch.SaveNormal();
+				#endif
 				
-				if (double(json["rating"]) < 3000){
+				S_SESSION rowSession;
+				Sql sqlInsert;
+				
+				String rang = ~json["rating"];
+				
+				sqlInsert*Insert(SESSION)(BATTLETAG, battletag)(RANK, rang)(DATE_SESSION, Date());
+				
+				/*
+				sql*Select(rowCompte).From(SESSION).Where(BATTLETAG == battletag);
+				Cout() << sql.ToString() << "\n";
+				
+				if (!sql.Fetch()){
+					Cout() << "Compte " + battletag + " introuvable, création necessaire.\n";
+					sql*Insert(SESSION)(BATTLETAG, battletag)(RANK, ~json["rating"])(DATE_SESSION, Date());
 				}
 				else{
-					bot.CreateMessage(channel, "");
+					Cout() << "Compte " + battletag + " recuperer, mise à jours.\n";
+						
+					bot.CreateMessage(channel, "Le rang actuel de " + ~sql[1] + " est de " + ~sql[2]);
 				}
-	        }
-	        
-	        bot.CreateMessage(channel,~json["rating"]);
+				*/
+				Sql sqlSelect;
+				
+				sqlSelect*Select(rowSession).From(SESSION).Where(BATTLETAG == battletag);
+				
+				bot.CreateMessage(channel, "Mise à jours de " + ~sqlSelect[1] + " : ELO " + ~sqlSelect[2]);
+				
+				
+				while(sqlSelect.Fetch()){
+					Cout() << ~sqlSelect[0] + " : BT=" + ~sqlSelect[1] + " ; RANK=" + ~sqlSelect[2] + " at " + ~sqlSelect[3] + "\n";
+				}
+ 	    	}/*
+	        else if(content.Find(1, '!')!=-1){
+	            bot.CreateMessage(channel, "Commande introuvable");
+	        }*/
  	    };
-    
-   
+ 	    
     bot.Listen();
 }
