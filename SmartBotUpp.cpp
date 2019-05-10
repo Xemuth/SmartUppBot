26d89fff357c514368c80f708528723d6beb8473
Natty <felix.soignon@gmail.com>
2019-05-10 16:09:12 +0200

    Schema qui marche

--- SmartBotUpp.cpp
+++ SmartBotUpp.cpp
@@ -1,6 +1,7 @@
 #include <Core/Core.h>
 #include <DiscordUpp/Discord.h>
 #include <SmartUppBot/sql_lite.h>
+#include <chrono>
 
 #ifdef _DEBUG
 #include <Sql/sch_schema.h>
@@ -14,17 +15,7 @@ CONSOLE_APP_MAIN {
 
     Discord bot("314391413200650250","MzE0MzkxNDEzMjAwNjUwMjUw.XLbeew.4-EvNJLFiPGMVoZ6s7pTnoqEObc");
     
-    bot.WhenMessageCreated = [&](ValueMap payload) {
-        Cout() << payload["d"]<<"\n";
-  		String channel  = payload["d"]["channel_id"];
-        String content  = payload["d"]["content"];
-        String userName = payload["d"]["author"]["username"];
-        String id = payload["d"]["author"]["id"];
-        String discriminator = payload["d"]["author"]["discriminator"];
-        
-        String battletag;
-        ValueMap json;
-        Cout() << "Debut programme\n";
+    Cout() << "Debut programme\n";
         
         // ----------- Instantiation SQL LITE 3 --------------------
         Sqlite3Session sqlite3;
@@ -52,79 +43,104 @@ CONSOLE_APP_MAIN {
 			}
 			sch.SaveNormal();
 		#endif
+    
+    bot.WhenMessageCreated = [&](ValueMap payload) {
+        Cout() << payload["d"]<<"\n";
+  		String channel  = payload["d"]["channel_id"];
+        String content  = payload["d"]["content"];
+        String userName = payload["d"]["author"]["username"];
+        String id = payload["d"]["author"]["id"];
+        String discriminator = payload["d"]["author"]["discriminator"];
+        
+        String battletag;
+        ValueMap json;
+        
         
         Cout() << "Debut traitement \n";
 	        
-	        //Affichage des stats d'un BattleTag
-	        if (content.Find("!stats")!=-1){
-	        	content.Remove(0, 7);
-	        	battletag = content;
-	       		
-			  	bot.CreateMessage(channel, "Recherche de stats pour " + battletag);
-	        		        	
-				HttpRequest query("https://ow-api.com/v1/stats/pc/eu/" + battletag + "/profile");
-				query.GET();
-				query.Timeout(10000);
+        //Affichage des stats d'un BattleTag
+        if (content.Find("!stats")!=-1){
+        	content.Remove(0, 7);
+        	battletag = content;
+       		
+		  	bot.CreateMessage(channel, "Recherche de stats pour " + battletag);
+        		        	
+			HttpRequest query("https://ow-api.com/v1/stats/pc/eu/" + battletag + "/profile");
+			query.GET();
+			query.Timeout(10000);
+			
+			json = ParseJSON(query.Execute());
+        	bot.CreateMessage(channel,json["rating"]);
+       
+        }//------------------ MAJ des stats d'un BattleTag ---------------------- 
+        else if(content.Find("!upd")!=-1){
+            S_SESSION rowSession;
+            	            
+            if (content.GetLength() == 4){ //Commande = !upd
+ 	      		Sql sqlGetBattletag;
+ 	      		
+ 	      		//------------------ Recherche du battletag sur la bdd ----------------------
+ 	      		sqlGetBattletag*Select(rowSession).From(SESSION).Where(DISCORD_NAME == userName);
 				
-				json = ParseJSON(query.Execute());
-	        	bot.CreateMessage(channel,json["rating"]);
-	       
-	        }//------------------ MAJ des stats d'un BattleTag ---------------------- 
-	        else if(content.Find("!upd")!=-1){
-	            S_SESSION rowSession;
-	            	            
-	            if (content.GetLength() == 4){ //Commande = !upd
-	 	      		Sql sqlGetBattletag;
-	 	      		
-	 	      		//------------------ Recherche du battletag sur la bdd ----------------------
-	 	      		sqlGetBattletag*Select(rowSession).From(SESSION).Where(DISCORD_NAME == userName);
-					
-					if (sqlGetBattletag.Fetch()){
-						Cout() << "Battletag trouvé\n";
-						battletag = ~sqlGetBattletag[1];
-					}
-					else{
-						Cout() << "Battletag non trouvé\n";
-						bot.CreateMessage(channel, "Aucun compte BattleNet n'est associé à votre nom discord, pour l'ajouter merci d'utiliser la commande '!upd [BattleTag]' en remplacant le '#' par un '-'.");
-						return;
-					}
-	            }
-	            else{//Commande = !upd NattyRoots-21691
-	                content.Remove(0, 5);
-	                battletag = content;
-	            }
- 	      		//-------------------------- REQUETE API OW ------------------------------	
- 	      		Cout() << "Requete API OW\n";
- 	      		        	
-	        	HttpRequest query("https://ow-api.com/v1/stats/pc/eu/" + battletag + "/profile");
-				query.GET();
-				query.Timeout(5000);
-				
-				json = ParseJSON(query.Execute());
-	        	
-				String rang = ~json["rating"];
-				
-				//----------------- INSERTION SESSION ---------------------------------------
-				Cout() << "Insertion nouvel ligne (" + battletag + ", " + userName + ", " + rang + ")\n";
-				
-				Sql sqlInsert;
-				sqlInsert*Insert(SESSION)(BATTLETAG, battletag)(DISCORD_NAME, userName)(RANK, rang);
-
-				Sql sqlSelect;
-				
-				sqlSelect*Select(rowSession).From(SESSION).Where(DISCORD_NAME == userName);
-				
-				bot.CreateMessage(channel, "Mise à jour de " + ~sqlSelect[1] + " : ELO " + ~sqlSelect[3]);
-				
-				while(sqlSelect.Fetch()){
-					Cout() << ~sqlSelect[0] + " : BT = " + ~sqlSelect[1] + " ; DISCORD =" + ~sqlSelect[2] + " ; ELO = " + ~sqlSelect[3] + /*" at " + ~sqlSelect[4] + */"\n";
+				if (sqlGetBattletag.Fetch()){
+					Cout() << "Battletag trouvé\n";
+					battletag = ~sqlGetBattletag[1];
 				}
-				
- 	    	}/*
-	        else if(content.Find(1, '!')!=-1){
-	            bot.CreateMessage(channel, "Commande introuvable");
-	        }*/
- 	    };
+				else{
+					Cout() << "Battletag non trouvé\n";
+					bot.CreateMessage(channel, "Aucun compte BattleNet n'est associé à votre nom discord, pour l'ajouter merci d'utiliser la commande '!upd [BattleTag]' en remplacant le '#' par un '-'.");
+					return;
+				}
+            }
+            else{//Commande = !upd NattyRoots-21691
+                content.Remove(0, 5);
+                battletag = content;
+            }
+      		//-------------------------- REQUETE API OW ------------------------------	
+      		Cout() << "Requete API OW\n";
+      		        	
+        	HttpRequest query("https://ow-api.com/v1/stats/pc/eu/" + battletag + "/profile");
+			query.GET();
+			query.Timeout(5000);
+			
+			json = ParseJSON(query.Execute());
+        	
+			String rang = ~json["rating"];
+			
+			//----------------- INSERTION SESSION ---------------------------------------
+			Cout() << "Insertion nouvel ligne (" + battletag + ", " + userName + ", " + rang + ")\n";
+
+			std::chrono::milliseconds ms = std::chrono::duration_cast< std::chrono::milliseconds >(
+			    std::chrono::system_clock::now().time_since_epoch()
+			);
+			
+			Sql sqlInsert;
+			sqlInsert*Insert(SESSION)(BATTLETAG, battletag)(DISCORD_NAME, userName)(RANK, rang)(DATE_SESSION, ms.count());
+
+			Sql sqlSelect;
+			
+			sqlSelect*Select(rowSession).From(SESSION).Where(DISCORD_NAME == userName).OrderBy(Descending(DATE_SESSION));
+			
+			bot.CreateMessage(channel, "Mise à jour de " + ~sqlSelect[1] + " : ELO " + ~sqlSelect[3]);
+			
+			while(sqlSelect.Fetch()){
+				Cout() << ~sqlSelect[0] + " : BT = " + ~sqlSelect[1] + " ; DISCORD =" + ~sqlSelect[2] + " ; ELO = " + ~sqlSelect[3] + /*" at " + ~sqlSelect[4] + */"\n";
+			}
+			
+    	}
+    	else if(content.Find("!leader")){
+    		/*Sql sqlSelect;
+    		S_SESSION rowSession;
+    		
+    		sqlSelect.*Select(rowSession).From*/
+    		
+    		SQL.Execute("SELECT DISCTINCT BATTLETAG FROM ")
+    	}/*
+        else if(content.Find(1, '!')!=-1){
+            bot.CreateMessage(channel, "Commande introuvable");
+        }*/
+        
+    };
  	    
     bot.Listen();
 }
