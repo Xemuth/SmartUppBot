5a014d8e10be98c3274e5e44f845379af1b8a07a
Xemuth <hamon.clement@outlook.fr>
2019-11-05 09:19:41 +0100

    bash

--- update.sh
+++ update.sh
@@ -1,30 +1,67 @@
 git pull
 cd ..
 
-cd Discord_Minecraft
-git pull
-cd ..
 
-cd Discord_Overwatch
-git pull
-cd ..
+FILE=./Discord_Minecraft
+if [ -f "$FILE" ]; then
+    cd Discord_Minecraft
+	git pull
+	cd ..
+else 
+    git clone https://github.com/Xemuth/Discord_Minecraft
+fi
 
-cd Discord_RNG
-git pull
-cd ..
+FILE=./Discord_Overwatch
+if [ -f "$FILE" ]; then
+	cd Discord_Overwatch
+	git pull
+	cd ..
+else 
+    git clone https://github.com/Xemuth/Discord_Overwatch
+fi
 
-cd Discord_WOW
-git pull
-cd ..
+FILE=./Discord_RNG
+if [ -f "$FILE" ]; then
+	cd Discord_RNG
+	git pull
+	cd ..
+else 
+    git clone https://github.com/Xemuth/Discord_RNG
+fi
 
-cd EasyConfiguration
-git pull
-cd ..
+FILE=./Discord_WOW
+if [ -f "$FILE" ]; then
+	cd Discord_WOW
+	git pull
+	cd ..
+else 
+    git clone https://github.com/Xemuth/Discord_WOW
+fi
 
-cd GraphBuilder
-git pull 
-cd ..
+FILE=./EasyConfiguration
+if [ -f "$FILE" ]; then
+	cd EasyConfiguration
+	git pull
+	cd ..
+else 
+    git clone https://github.com/Xemuth/EasyConfiguration
+fi
 
-cd RconManager
-git pull
-cd ..
+FILE=./GraphBuilder
+if [ -f "$FILE" ]; then
+	cd GraphBuilder
+	git pull 
+	cd ..
+else 
+    git clone https://github.com/Xemuth/GraphBuilder
+fi
+
+
+FILE=./RconManager
+if [ -f "$FILE" ]; then
+	cd RconManager
+	git pull
+	cd ..
+else 
+    git clone https://github.com/Xemuth/RconManager
+fi
