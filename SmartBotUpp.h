43678cf1b916ab595a449a30a4fd1502838d5201
Xemuth <hamon.clement@outlook.fr>
2019-11-06 11:53:21 +0100

    Maj Multi prefix

--- SmartBotUpp.h
+++ SmartBotUpp.h
@@ -85,6 +85,7 @@ class DiscordModule{
 		String ChannelLastMessage=""; //hook to latest message chan 
 		String NameOfFunction="";
 		Vector<String> MessageArgs;
+		ArrayMap<String,String>
 		
 	    String AuthorId =""; 
 	    String Message ="";
@@ -109,13 +110,20 @@ class DiscordModule{
 		void ClearMessageArgs();
 		
 		Upp::String name="";
-		Upp::String prefix="";
+		//Upp::String prefix="";
+		Vector<Upp::String> prefix;
+		
+		
 		
 		Upp::Array<Event<ValueMap>> EventsMapMessageCreated{ 
 			[&](ValueMap e){if(NameOfFunction.IsEqual("help"))Help(e);},
 			[&](ValueMap e){if(NameOfFunction.IsEqual("credit"))Credit(e);}
 		};
 		
+		
+		String& AddPrefix(String _prefix);
+		bool AddPrefix(Vector<String>& _prefix);
+		bool RemovePrefix(String _prefix);
 	
 		virtual bool goodPrefix(Upp::String prefixToTest);
 		virtual void EventsMessageCreated(ValueMap payload);
